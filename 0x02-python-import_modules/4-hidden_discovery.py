771fc119275c9f0c02920da501ed29a377c4f2bb
ajuwong007 <ajuwong007@gmail.com>
2023-01-05 19:32:05 +0000

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# 5-variable_load.py
+# 4-hidden_discovery.py
 # Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-    print(a)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
