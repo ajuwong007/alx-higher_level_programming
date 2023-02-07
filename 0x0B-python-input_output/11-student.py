ceb334c7daefe88cae3c24242b88029b285abad0
ajuwong007 <ajuwong007@gmail.com>
2023-02-07 17:44:51 +0000

    all task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
-"""
-11-student.py
-Public method def reload_from_json(self, json):
-that replaces all attributes of the Student instance
+"""Student
 """
 
 
 class Student:
+    """Contains student data
     """
-    class student and def, nomb, apellido, age
-    """
-    de __init__(self, first_name, last_name, age):
 
-       """inicialization"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-       self.first_name = first_name
-
-       self.last_name = last_name
-
-       self.age = age
-
-    def to_json(self, attrs=None):
-        """dictionary representation
-        attrs: list of attributes
-        return: dict representation of instance
+    def to_json(self):
+        """Retrieves dictionary of Student
         """
-        my_dict = dict()
-        if type(attrs) is list and all(type(x) is str for x in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dict.update({x: self.__dict__[x]})
-            return my_dict
-        return self.__dict__.copy()
 
-    def reload_from_json(self, json):
-        """
-        Replaces all attributes of the Student instance.
-        """
-        for x in json:
-            self.__dict__.update({x: json[x]})
+        return self.__dict__
